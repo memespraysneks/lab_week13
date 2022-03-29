40b7d30a5588cea399f17168eeed78ed0ad5ab97
Caleb Seeman <caleb@seeman.com>
2022-03-29 15:23:44 -0700

    Fixed some small issues with part 1 and began styling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 //part 1
 const logging = document.querySelector(".original-container")
-
+const cloneArea = document.querySelector(".copy-container")
 logging.addEventListener("click", (evt) => {
-    const cloneArea = document.querySelector(".copy-container")
-    cloneArea.insertAdjacentHTML("beforeend", evt.target.innerHTML)
+    if (evt.target.localName != "div.copy-container") {
+        cloneArea.insertAdjacentHTML("beforeend", evt.target.innerHTML)
+    }
 });
 //part 2
 const btn = document.querySelector("button")
 
-btn.addEventListener("click", (evt) => {
-    const cloneArea = document.querySelector(".copy-container")
+btn.addEventListener("click", () => {
     cloneArea.innerHTML = ''
 })
-//part 3
